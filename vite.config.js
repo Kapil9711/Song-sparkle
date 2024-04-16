10cbe8dfc2ac82c875a7f387ee0fce0f5bceae4e
kapil <kapil.personal9711@gmail.com>
2024-04-16 12:01:34 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/youtubeclientv2/.",
+  base: "/Song-sparkle/.",
   plugins: [react()],
 });
