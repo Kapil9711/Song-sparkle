4d799639d9be7b90e82fe432abaad41e8e0326a2
kapil <kapil.personal9711@gmail.com>
2024-04-19 18:20:09 +0530

    aa

--- src/components/card-list/CardList.component.jsx
+++ src/components/card-list/CardList.component.jsx
@@ -47,7 +47,7 @@ const CardList = ({
           color: "white",
           textAlign: "center",
           marginBottom: "30px",
-          fontSize: "1.3rem",
+          fontSize: "1.4rem",
         }}
       >
         Favorite Songs List From All Users
